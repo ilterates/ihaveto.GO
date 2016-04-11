24b0bbb94084ab7c4d697814d70e10abb1571359
ilterates <iltates@gmail.com>
2016-04-10 23:07:44 -0700

    moved add button + style change for map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -71,14 +71,16 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById('mapid'), {
     zoom: zoomVal,
     center: {lat: 37.774, lng: -122.431},
-    styles: [{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},
-    {"featureType":"transit","elementType":"labels","stylers":[{"visibility":"off"}]},
-    {"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},
-    {"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},
-    {"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
-    {"stylers":[{"hue":"#00aaff"},{"saturation":-100},{"gamma":2.15},{"lightness":12}]},
-    {"featureType":"road","elementType":"labels.text.fill","stylers":[{"visibility":"on"},
-    {"lightness":24}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":57}]}]
+    styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},
+    {"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},
+    {"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},
+    {"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},
+    {"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},
+    {"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},
+    {"featureType":"administrative.province","stylers":[{"visibility":"off"}]},
+    {"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},
+    {"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},
+    {"lightness":-25},{"saturation":-97}]}]
   });
   geocoder = new google.maps.Geocoder();
   $('.pure-button').click(function(e) {
