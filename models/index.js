42bfff4d3ada89f1781208f64384ea3209ebea63
ilterates <iltates@gmail.com>
2016-04-12 09:34:00 -0700

    heroku attempt 2

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-
+mongoose.connect("mongodb://localhost/ihavetogo");
 mongoose.connect( process.env.MONGOLAB_URI ||
                       process.env.MONGOHQ_URL ||
                       "mongodb://localhost/ihavetogo" );
