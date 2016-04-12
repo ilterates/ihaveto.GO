dcc37bf35d33040d60e0724d634413ee414a7dde
ilterates <iltates@gmail.com>
2016-04-12 09:26:36 -0700

    heroku attempt 1

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/ihavetogo");
 
+mongoose.connect( process.env.MONGOLAB_URI ||
+                      process.env.MONGOHQ_URL ||
+                      "mongodb://localhost/ihavetogo" );
 module.exports.Toilet = require("./toilet.js");
 module.exports.Rating = require("./rating.js");
